7493765af1e1a1ae061a86a92d6232093946bf89
Lucas <l.lopes.fga@gmail.com>
2019-11-10 22:33:50 -0200

    Retorna username no serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Usuario
-
+from django.contrib.auth.models import User
 class UsuarioSerializer(serializers.ModelSerializer):
-
+    user =  serializers.SerializerMethodField()
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = "__all__"
+    def get_user(self,obj):
+        return obj.user.username
         
